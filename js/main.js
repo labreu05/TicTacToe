f0f0b6b22d656fc1a078326d4bd23866f3f3618e
Leonardo Abreu <labreu@cincinnatus.edu.do>
2016-09-05 15:45:54 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// alert("Hello, whats your name?");
+
+// piece = document.querySelectorAll("h1");
+// piece = document.getElementsByTagName("h1");
+
+document.addEventListener("click", function(){
+	alert("clicked");
+});
